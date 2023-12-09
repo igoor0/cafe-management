7b4aec51d89b5bf91e07f46f5ff3f4aba1b9a4cb
Igoor0 <igor.kohsin@gmail.com>
2023-12-09 12:29:31 +0100

    Implement CRUD operations for Order entity
    - Add Order entity with necessary fields
    - Implement OrderService for business logic
    - Create OrderController for handling HTTP requests
    - Update Order for database operations

---
+++ server/src/main/java/dev/cafemanagement/server/model/Order.java
@@ -0,0 +1,34 @@
+package dev.cafemanagement.server.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Order {
+
+    @Id
+    @Getter
+    @GeneratedValue
+    private Long orderId;
+    private LocalDateTime orderDateTime;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItems> orderItemsList;
+    private double totalAmount;
+
+
+}
