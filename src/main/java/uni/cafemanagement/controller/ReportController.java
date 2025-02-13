d9f784ddf7a98c8afd7216a6adde65cddb240280
Igor Kohsin <igor.kohsin@icloud.com>
2025-02-13 22:03:25 +0100

    Resolve proper returns from Report endpoints

--- src/main/java/uni/cafemanagement/controller/ReportController.java
+++ src/main/java/uni/cafemanagement/controller/ReportController.java
@@ -1,11 +1,14 @@
 package uni.cafemanagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uni.cafemanagement.model.Report;
 import uni.cafemanagement.service.ReportService;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
 @RestController
@@ -20,32 +23,39 @@ public class ReportController {
     }
 
     @GetMapping("/daily")
-    public Report generateDailyReport() {
-        return reportService.generateDailyReport();
+    public ResponseEntity<Report> generateDailyReport() {
+        return ResponseEntity.ok(reportService.generateDailyReport());
     }
 
     @GetMapping("/monthly")
-    public Report generateMonthlyReport() {
-        return reportService.generateMonthlyReport();
+    public ResponseEntity<Report> generateMonthlyReport() {
+        return ResponseEntity.ok(reportService.generateMonthlyReport());
     }
 
     @GetMapping("/yearly")
-    public Report generateYearlyReport() {
-        return reportService.generateYearlyReport();
+    public ResponseEntity<Report> generateYearlyReport() {
+        return ResponseEntity.ok(reportService.generateYearlyReport());
     }
 
     @GetMapping("/general")
-    public Report generateGeneralReport() {
-        return reportService.generateGeneralReport();
+    public ResponseEntity<Report> generateGeneralReport() {
+        return ResponseEntity.ok(reportService.generateGeneralReport());
     }
 
     @GetMapping("/specific")
-    public Report generateSpecificDateReport(@RequestParam("date") String date) {
-        return reportService.generateSpecificDateReport(LocalDate.parse(date));
+    public ResponseEntity<Report> generateSpecificDateReport(@RequestParam("date") String date) {
+        try {
+            LocalDate parsedDate = LocalDate.parse(date);
+            Report report = reportService.generateSpecificDateReport(parsedDate);
+            return ResponseEntity.ok(report);
+        } catch (DateTimeParseException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping
-    public List<Report> getAllReports() {
-        return reportService.getAllReports();
+    public ResponseEntity<List<Report>> getAllReports() {
+        List<Report> reports = reportService.getAllReports();
+        return reports.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(reports);
     }
 }
